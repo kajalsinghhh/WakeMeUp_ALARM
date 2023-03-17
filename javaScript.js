320324aa077195e17bb3afd126c4f2756f640f48
Kajal singh <105795472+kajalsinghhh@users.noreply.github.com>
2023-03-17 13:12:20 +0530

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -1,18 +1,25 @@
 var hour = document.getElementById("hour");
 var minute = document.getElementById("minute");
 var second = document.getElementById("second");
+var time = document.getElementById("time");
+var setTime = () => {
 
-var setTime = () =>{
     var d = new Date();
-    h = (d.getHours()%12)*30;
-    m = d.getMinutes()*6;
-    s = d.getSeconds()*6;
- 
-    hour.style.transform = `rotate(${h+Math.floor(m/72)*6}deg)`;
-    minute.style.transform = `rotate(${m}deg)`;
-    second.style.transform = `rotate(${s}deg)`;
+
+    am_pm = d.getHours() > 12 ? "PM" : "AM";
+    h = (d.getHours() == 12) ? 12 : (d.getHours()%12);
+    m = d.getMinutes();
+    s = d.getSeconds();
+
+    time.innerHTML = ` ${h} : ${m} : ${s} ${am_pm}`
+    hour.style.transform = `rotate(${h * 30 + Math.floor(m / 16) * 6}deg)`;
+    minute.style.transform = `rotate(${m * 6}deg)`;
+    second.style.transform = `rotate(${s * 6}deg)`;
+
 }
 setTime();
 setInterval(() => {
     setTime();
 }, 1000);
+
+
